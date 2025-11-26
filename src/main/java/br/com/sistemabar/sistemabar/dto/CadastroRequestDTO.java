96ae38f6b03bcbd14338e8ef42a34100be9217c8
hanrri <hanrrikleber3666@gmail.com>
2025-11-26 19:40:30 -0300

    front-end

--- src/main/java/br/com/sistemabar/sistemabar/dto/CadastroRequestDTO.java
+++ src/main/java/br/com/sistemabar/sistemabar/dto/CadastroRequestDTO.java
@@ -2,6 +2,5 @@ package br.com.sistemabar.sistemabar.dto;
 
 import br.com.sistemabar.sistemabar.model.PerfilUsuario;
 
-// DTO para o request de /cadastrar
 public record CadastroRequestDTO(String login, String senha, PerfilUsuario perfil) {
 }
